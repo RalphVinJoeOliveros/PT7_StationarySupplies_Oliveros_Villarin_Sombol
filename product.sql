b8369b3569a099d05dfa5ecbd3081937f72181e9
Ralph Vin Joe Oliveros <oliveros.ralphvinjoe@ici.edu.ph>
2022-11-28 22:47:13 +0800

    sequel change table

--- product.sql
+++ product.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2022 at 03:45 PM
+-- Generation Time: Nov 28, 2022 at 03:46 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,32 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Table structure for table `product`
 --
 
-CREATE TABLE `customers` (
-  `Identifier` bigint(80) NOT NULL,
-  `Customer_Name` varchar(80) NOT NULL,
-  `Phone_Number` bigint(80) DEFAULT NULL,
-  `Delivery_Address` text DEFAULT NULL
+CREATE TABLE `product` (
+  `Unique_ID` bigint(80) NOT NULL,
+  `Product_Name` varchar(80) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `Price_Tag` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `customers`
+-- Dumping data for table `product`
 --
 
-INSERT INTO `customers` (`Identifier`, `Customer_Name`, `Phone_Number`, `Delivery_Address`) VALUES
-(45345345345, 'Ivanne Karl Sombol', 9631360901, 'Paitan, Iligan City');
+INSERT INTO `product` (`Unique_ID`, `Product_Name`, `Description`, `Price_Tag`) VALUES
+(346545645, 'Ball Pen', 'Made by Microsoft Corporation', '45.00'),
+(45645645646, 'Notebook', 'Made by Sony', '54.25'),
+(567567567567, 'Ball Pen', 'Made by Microsoft Corporation', '45.12');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `customers`
+-- Indexes for table `product`
 --
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`Identifier`);
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`Unique_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
